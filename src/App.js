c3f53b62e451b46d25e0c4a7d381eae52cff05e6
Umut Can Yılmaz <umutcannyilmaz@gmail.com>
2022-11-07 17:58:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='navbar'> NAVBAR</div>
+      <div className='sliderContainer'>SLIDER</div>
+      <div className='boxes'>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+      </div>
+      <div className='differentboxes'>
+        <div className='db2'></div>
+        <div className='db1'></div>
+      </div>
+      <div className='wrapper'>
+        <ul>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+          <li><img src='https://via.placeholder.com/150'></img></li>
+        </ul>
+      </div>
+
+      <div className='footer'>
+        <div className='footeritem'>
+          <ul>
+            <li>Hakkımızda</li>
+            <li>Hakkımızda</li>
+            <li>Hakkımızda</li>
+            <li>Hakkımızda</li>
+            <li>Hakkımızda</li>
+
+          </ul>
+        </div>
+        <div className='footeritem'></div>
+        <div className='footeritem'></div>
+        <div className='footeritem'></div>
+        <div className='footeritem'></div>
+      </div>
+
+      
+      <div className='yüzde'>
+        <div className='yüzdeitem'>Text Center</div> 
+        <div className='yüzdeitem'>Text Center</div> 
+        </div>
     </div>
+
+
+
+
   );
 }
 
